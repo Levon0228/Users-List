8829154bfd91a8b3dd42482cae7dd811a5f2ef56
Levon0228 <levonhar8yan@gmail.com>
2021-01-17 21:59:49 +0400

    fix comments redering bug

--- src/Containers/Posts/Comments/CommentsContanier.jsx
+++ src/Containers/Posts/Comments/CommentsContanier.jsx
@@ -2,15 +2,11 @@
 
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
-//import { Context } from "../../../Context/Context";
 import { API_URL } from "../../../config/index";
 import Comments from "./Comments";
 
 const CommentsContanier = (props) => {
   const [comments, setComments] = useState([]);
-  //const [isShowComments, setShow] = useState(false)
- // if (context) setShow(context)
   console.log("postId", props.postId)
   
  useEffect(() => {
--- src/Containers/Posts/PostsContanier.jsx
+++ src/Containers/Posts/PostsContanier.jsx
@@ -3,15 +3,12 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { Context } from "../../Context/Context";
 import { API_URL } from "../../config/index";
 import Posts from "./Posts";
 
 const PostsContanier = () => {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
- // const [context, setContext] = useContext(Context);
-  //const userName = context.name;
   const [postId, setpostId] = useState(null);
   const [isOpen, setisOpen] = useState(false);
 
